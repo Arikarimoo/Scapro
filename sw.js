701a2385c7a3c60785eb3c8f3b0eb8e932e9a776
Arikarimoo <158246210+Arikarimoo@users.noreply.github.com>
2026-03-26 22:13:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 
-self.addEventListener('push', (event) => {
-    const options = {
-        body: event.data ? event.data.text() : "Ново возило во системот!",
-        icon: 'Icon.jpg',
-        badge: 'Icon.jpg',
-        vibrate: [200, 100, 200]
-    };
-    event.waitUntil(self.registration.showNotification("Scania Pro", options));
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then(clients => {
+            if (clients.length > 0) return clients[0].focus();
+            return clients.openWindow('/');
+        })
+    );
 });
